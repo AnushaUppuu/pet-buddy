4defc799b3f9ced3bb79e1435115e2ba74a335ea
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-21 15:06:31 +0530

    Create the connection file to connect to the database

---
+++ Backend/connection.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export async function connection() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/PetBuddy-2');
+    console.log('Connected to the database');
+  } catch (e) {
+    console.log('Unable to connect');
+  }
+}
