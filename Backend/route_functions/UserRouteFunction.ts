a50a378df5d3cc52d9c83a112e3365235bb301ab
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-21 16:08:43 +0530

    Update the create user function

--- Backend/route_functions/UserRouteFunction.ts
+++ Backend/route_functions/UserRouteFunction.ts
@@ -1,4 +1,15 @@
 import express, {Request,Response} from 'express'
+import { user } from '../schema/User'
 export const createUser=async(req:Request,res:Response):Promise<any>=>{
-    res.send("Create user route")
+    try{
+        const searchUser=await user.findOne({username:req.body.username});
+        if(searchUser){
+            await user.create(req.body);
+            res.status(200).send("User successfully added");
+        }else{
+            res.status(202).send("Username already exists");
+        }
+    }catch(e){
+        res.status(500).send("Error while creation")
+    }
 }
