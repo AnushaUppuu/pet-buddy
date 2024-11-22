f29400e181cd9c66069207e85b5dbb7e0921e09c
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-22 19:43:43 +0530

    Update the tests for stack navigator to bottom tab navigation

--- __tests__/screen_tests/SingnedInStack.test.tsx
+++ __tests__/screen_tests/SingnedInStack.test.tsx
@@ -2,11 +2,12 @@ import { NavigationContainer } from "@react-navigation/native"
 import { render } from "@testing-library/react-native"
 import SingInStack from "../../src/screens/SingInStack"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-jest.mock('@react-navigation/native-stack',()=>({
-    createNativeStackNavigator:jest.fn().mockImplementation(()=>({
-        Navigator:jest.fn(),
-        Screen:jest.fn()
-    }))
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+jest.mock('@react-navigation/bottom-tabs',()=>({
+   createBottomTabNavigator:jest.fn().mockImplementation(()=>({
+    Navigator:jest.fn(),
+    Screen:jest.fn()
+   }))
 }))
 describe("Signed In Stack ",()=>{
     it("renders the elements correctly",()=>{
