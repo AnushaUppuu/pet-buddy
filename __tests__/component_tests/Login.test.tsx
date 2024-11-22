67461e16ef2b68c1b7e75c68f9467ebcab57c0dc
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-22 17:26:17 +0530

    Update the login test, add fetching test case

--- __tests__/component_tests/Login.test.tsx
+++ __tests__/component_tests/Login.test.tsx
@@ -1,5 +1,15 @@
-import {fireEvent, render, screen} from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
 import Login from '../../src/components/Login';
+import {act} from 'react-test-renderer';
+import {
+  GlobalContext,
+  GlobalContextProvider,
+} from '../../src/context/GlobalContext';
 
 const mockedNavigate = jest.fn();
 const mockedGoBack = jest.fn();
@@ -18,7 +28,16 @@ jest.mock('@react-navigation/native', () => {
 });
 describe('Login component', () => {
   beforeEach(() => {
-    render(<Login />);
+    const username = 'Anusha_uppu';
+    const setUsername = jest.fn();
+    const petname = 'Cooper';
+    const setPetName = jest.fn();
+
+    render(
+      <GlobalContextProvider>
+        <Login />
+      </GlobalContextProvider>,
+    );
   });
   it('Renders the elements correctly', () => {
     expect(screen.getByTestId('username')).toBeTruthy();
@@ -29,5 +48,29 @@ describe('Login component', () => {
   it('Should navigate to the register screen upon clicking on the register', () => {
     fireEvent.press(screen.getByTestId('register-text'));
     expect(mockedNavigate).toHaveBeenCalledWith('Register');
+  });
+  it('Fetch is called when the login button is clicked', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({success: true}),
+      }),
+    ) as jest.Mock;
+    await waitFor(async () => {
+      fireEvent.changeText(screen.getByTestId('username'), 'Anusha_uppu');
+      fireEvent.changeText(screen.getByTestId('password'), 'anu@123');
+      fireEvent.press(screen.getByTestId('login-button'));
+    });
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      'http://localhost:4000/users/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({username: 'Anusha_uppu', password: 'anu@123'}),
+      },
+    );
   });
 });
