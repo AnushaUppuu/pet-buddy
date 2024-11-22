5addf05d78db138aac5be8c45096788e3e998389
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-22 22:36:56 +0530

    Create the Sign In component which contains the screens that will be shown when the user is logged in

---
+++ src/screens/SingInStack.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+import Home from './Home';
+import Service from './Service';
+import Training from './Training';
+
+function SingInStack() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Service" component={Service} />
+      <Tab.Screen name="Training" component={Training} />
+    </Tab.Navigator>
+  );
+}
+
+export default SingInStack;
