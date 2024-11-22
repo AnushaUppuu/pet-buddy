4e0d3e8b96f4a6d6ea593a782a138e8997f1a758
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-22 17:25:31 +0530

    Add the context provider

--- App.tsx
+++ App.tsx
@@ -5,23 +5,28 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import Starting from './src/components/Starting';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SingnedOutStack from './src/screens/SingnedOutStack';
 import SingInStack from './src/screens/SingInStack';
+import { GlobalContext, GlobalContextProvider } from './src/context/GlobalContext';
 const Stack=createNativeStackNavigator();
 function App(): React.JSX.Element {
+  const [username,setUsername]=useState('');
+  const [petname,setPetName]=useState('');
   return (
+    <GlobalContextProvider>
     <NavigationContainer>
      <Stack.Navigator>
       <Stack.Screen name='SingnedOut' component={SingnedOutStack} options={{headerShown:false}}/>
       <Stack.Screen name='SingnedIn' component={SingInStack} options={{headerShown:false}}/>
      </Stack.Navigator>
     </NavigationContainer>
-    
+    </GlobalContextProvider>
+ 
   );
 }
 
