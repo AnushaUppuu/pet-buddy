7dbae8b4f38d3523f7c49e7f5d89ae3262fd3b75
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-24 22:09:48 +0530

    Create tests for the get all pets route

--- __tests__/backend_tests/PetRoute.test.ts
+++ __tests__/backend_tests/PetRoute.test.ts
@@ -1,9 +1,10 @@
 import { app } from "../../Backend/server";
 import express from 'express';
 import request from 'supertest';
-import { createPet } from "../../Backend/route_functions/PetRouteFunctions";
+import { createPet, getAllPets } from "../../Backend/route_functions/PetRouteFunctions";
 jest.mock('../../Backend/route_functions/PetRouteFunctions',()=>({
     createPet:jest.fn(),
+    getAllPets:jest.fn(),
 }))
 describe("Create pet route",()=>{
     it("Should return success message upon adding pet to the database successfully",async()=>{
@@ -38,4 +39,36 @@ describe("Create pet route",()=>{
         expect(result.status).toBe(404);
         expect(result.text).toBe("User not found");
     });
+})
+describe("Get all pets",()=>{
+    it("Should return the pets data",async()=>{
+        const value=[
+            {
+                name:"Cooper",
+                weight:4,
+            }
+        ];
+        (getAllPets as jest.Mock).mockImplementation((req,res)=>{
+            res.status(200).json(value);
+        })
+        const result=await request(app).get('/pets/getAllPets/Anusha_uppu');
+        expect(result.status).toBe(200);
+        expect(result.body).toEqual(value)
+    })
+    it("Should return the error message when an error occurs",async()=>{
+        (getAllPets as jest.Mock).mockImplementation((req,res)=>{
+            res.status(500).send("Database error");
+        })
+        const result=await request(app).get('/pets/getAllPets/Anusha_uppu');
+        expect(result.status).toBe(500);
+        expect(result.text).toBe("Database error")
+    })
+    it("Should return the error message when an error occurs",async()=>{
+        (getAllPets as jest.Mock).mockImplementation((req,res)=>{
+            res.status(404).send("User not found");
+        })
+        const result=await request(app).get('/pets/getAllPets/Anusha_uppu');
+        expect(result.status).toBe(404);
+        expect(result.text).toBe("User not found")
+    })
 })
