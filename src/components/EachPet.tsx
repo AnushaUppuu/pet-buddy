4524e6981214a15d50b92e320763e26493c931db
AnushaUppuu <anusha.uppu@everest.engineering>
2024-11-25 22:31:42 +0530

    Create each pet component to render the pet images and name in the my pets component

---
+++ src/components/EachPet.tsx
@@ -0,0 +1,54 @@
+import React, {useContext, useEffect} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {TPet} from '../types/TPet';
+import { useNavigation } from '@react-navigation/native';
+import { GlobalContext } from '../context/GlobalContext';
+
+function EachPet({pet}: any) {
+ const {petname,setPetName}=useContext(GlobalContext)
+  const nav=useNavigation<any>();
+  return (
+    <View style={styles.mainContainer}>
+      <TouchableOpacity testID='pet-image' onPress={()=>{
+        setPetName(pet.item.name);
+        nav.navigate('Pet')}}>
+        <Image style={styles.image} source={{uri: pet.item.petImage}} />
+      </TouchableOpacity>
+      <View style={styles.details}>
+        <Text testID='name' style={styles.name}>{pet.item.name}</Text>
+        <Text testID='breed'>{pet.item.breed}</Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  mainContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    // justifyContent:'center',
+    padding: 10,
+  },
+  image: {
+    width: 130,
+    height: 150,
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    borderColor: 'white',
+  },
+  details: {
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    borderColor: 'white',
+    paddingVertical: 25,
+    paddingRight: 15,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 17,
+    alignSelf: 'flex-start',
+  },
+});
+export default EachPet;
