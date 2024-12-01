8fa73189088a3f078a4626d1c21332109cb09240
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-01 18:37:47 +0530

    Add petdata to the context to the know the current selected pet

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,19 +1,24 @@
 import React, {Children, createContext, useState} from 'react';
+import { TPet } from '../types/TPet';
 
 type TGlobal = {
   username: string;
   setUsername: React.Dispatch<React.SetStateAction<string>>;
   petname: string;
   setPetName: React.Dispatch<React.SetStateAction<string>>;
+  petdata:TPet|undefined;
+  setPetData:React.Dispatch<React.SetStateAction<TPet|undefined>>;
 };
 export const GlobalContext = createContext<TGlobal>({} as TGlobal);
 
 export const GlobalContextProvider = ({children}:any) => {
   const [username, setUsername] = useState('');
   const [petname, setPetName] = useState('');
+  const [petdata,setPetData]=useState<TPet|undefined>()
+ 
   return (
     <GlobalContext.Provider
-      value={{username, setPetName, setUsername, petname}}>
+      value={{username, setPetName, setUsername, petname,petdata,setPetData}}>
       {children}
     </GlobalContext.Provider>
   );
