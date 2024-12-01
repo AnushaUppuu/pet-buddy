1829cc78c4ca288c59a5940392008c3435a0569b
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-01 19:36:14 +0530

    Modify the connection testing, close the connection after every test

--- __tests__/backend_tests/DatabaseConnection.test.ts
+++ __tests__/backend_tests/DatabaseConnection.test.ts
@@ -1,14 +1,29 @@
-import {connection} from '../../Backend/connection';
-jest.mock('../../Backend/connection', () => ({
+import {connection, closeConnection} from '../../Backend/connection';
+import mongoose, {connect} from 'mongoose';
+
+// jest.mock('../../Backend/connection', () => ({
+//   connection: jest.fn(),
+// }));
+jest.mock('mongoose', () => ({
+  connect: jest.fn(),
   connection: jest.fn(),
 }));
 describe('Database connection', () => {
-  it('Succuss message upon connecting', async () => {
-    (connection as jest.Mock).mockResolvedValue('Connection successful');
-    expect(await connection()).toBe('Connection successful');
+  beforeEach(async () => {
+    console.log = jest.fn();
+    await connection();
   });
-  it('Should display error message upon failed connection', async() => {
-    (connection as jest.Mock).mockResolvedValue('Unable to connect');
-    expect(await connection()).toBe('Unable to connect');
-  })
+  afterEach(async () => {
+    await closeConnection();
+  });
+  it('Succuss message upon connecting', async () => {
+    (mongoose.connect as jest.Mock).mockResolvedValueOnce(true);
+    expect(console.log).toHaveBeenCalledWith('Connected to the database');
+  });
+  it('Should display error message upon failed connection', async () => {
+    (connect as jest.Mock).mockRejectedValueOnce(
+      new Error('Connection failed'),
+    );
+    expect(console.log).toHaveBeenCalledWith('Unable to connect');
+  });
 });
