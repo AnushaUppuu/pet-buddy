dbed6d0bac428bf562899afb4a26d4a3a4b707a7
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-02 23:59:52 +0530

    Update the add activity route

--- Backend/routes/pet_routes.ts
+++ Backend/routes/pet_routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { addRemainder, createPet, getAllPets, getSinglePet } from '../route_functions/PetRouteFunctions';
+import { addActivity, addRemainder, createPet, getAllPets, getSinglePet } from '../route_functions/PetRouteFunctions';
 const router=express.Router();
 router.post('/addPet',createPet);
 router.get('/getAllPets/:username',getAllPets);
 router.get('/getSinglePet/:username/:petname',getSinglePet);
 router.post('/addRemainder/:username/:petname',addRemainder);
+router.post('/addActivity/:username/:petname',addActivity);
 export default router
