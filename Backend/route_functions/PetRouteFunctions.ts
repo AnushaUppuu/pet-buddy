491c8af492446722788337630f3b1c2ccfb96906
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-03 17:15:49 +0530

    Modify the indetention and remove unneccesary lines

--- Backend/route_functions/PetRouteFunctions.ts
+++ Backend/route_functions/PetRouteFunctions.ts
@@ -11,19 +11,19 @@ export const createPet = async (req: Request, res: Response): Promise<any> => {
       if (searchPet) {
         res.status(201).send('Pet already exist');
       } else {
-        const value={
-          name:req.body.name,
-          age:req.body.age,
-          weight:req.body.weight,
-          height:req.body.height,
-          color:req.body.color,
-          remarks:req.body.remarks,
-          owner:searchUser._id,
-          gender:req.body.gender,
-          category:req.body.category,
-          breed:req.body.breed,
-          emergencyContact:req.body.emergencyContact,
-          profileImage:req.body.profileImage
+        const value = {
+          name: req.body.name,
+          age: req.body.age,
+          weight: req.body.weight,
+          height: req.body.height,
+          color: req.body.color,
+          remarks: req.body.remarks,
+          owner: searchUser._id,
+          gender: req.body.gender,
+          category: req.body.category,
+          breed: req.body.breed,
+          emergencyContact: req.body.emergencyContact,
+          profileImage: req.body.profileImage,
         };
         await pet.create(value);
         res.status(200).send('Pet successfully added');
@@ -72,44 +72,58 @@ export const getSinglePet = async (
     res.status(500).send('Database error');
   }
 };
-export const addRemainder=async(req:Request,res:Response):Promise<any>=>{
-  const {username,petname}=req.params;
-   try{
+export const addRemainder = async (
+  req: Request,
+  res: Response,
+): Promise<any> => {
+  const {username, petname} = req.params;
+  try {
     const searchUser = await user.findOne({username: username});
-    if(searchUser){
-      const searchPet=await pet.findOne({$and:[{name:petname},{owner:searchUser._id}]});
-      if(searchPet){
-        await pet.updateOne({_id:searchPet._id},{$push:{remainders:req.body}});
-        res.status(200).send("Remainder added successfully");
+    if (searchUser) {
+      const searchPet = await pet.findOne({
+        $and: [{name: petname}, {owner: searchUser._id}],
+      });
+      if (searchPet) {
+        await pet.updateOne(
+          {_id: searchPet._id},
+          {$push: {remainders: req.body}},
+        );
+        res.status(200).send('Remainder added successfully');
+      } else {
+        res.status(201).send('Pet not found');
       }
-      else{
-        res.status(201).send("Pet not found");
-      }
-    }else{
-      res.status(404).send("User not found")
+    } else {
+      res.status(404).send('User not found');
     }
-   }catch(e){
-    res.status(500).send("Error while adding")
-   }
-}
-export const addActivity=async(req:Request,res:Response):Promise<any>=>{
-  const {username,petname}=req.params;
-  console.log(req.body);
-  try{
-    const searchUser = await user.findOne({username: username});
-    if(searchUser){
-      const searchPet=await pet.findOne({$and:[{name:petname},{owner:searchUser._id}]});
-      if(searchPet){
-        await pet.updateOne({_id:searchPet._id},{$push:{activity:req.body}});
-        res.status(200).send("Remainder added successfully");
-      }
-      else{
-        res.status(201).send("Pet not found");
-      }
-    }else{
-      res.status(404).send("User not found")
-    }
-  }catch(e){
-    res.status(500).send("Error while adding")
+  } catch (e) {
+    res.status(500).send('Error while adding');
   }
-}
+};
+export const addActivity = async (
+  req: Request,
+  res: Response,
+): Promise<any> => {
+  const {username, petname} = req.params;
+  console.log(req.body);
+  try {
+    const searchUser = await user.findOne({username: username});
+    if (searchUser) {
+      const searchPet = await pet.findOne({
+        $and: [{name: petname}, {owner: searchUser._id}],
+      });
+      if (searchPet) {
+        await pet.updateOne(
+          {_id: searchPet._id},
+          {$push: {activity: req.body}},
+        );
+        res.status(200).send('Activity added successfully');
+      } else {
+        res.status(201).send('Pet not found');
+      }
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (e) {
+    res.status(500).send('Error while adding');
+  }
+};
