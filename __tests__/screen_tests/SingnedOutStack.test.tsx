519a32a4a6e1f7c406e1da2cf3aebb288a7c6b05
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-03 17:51:04 +0530

    Mock the image picker package

--- __tests__/screen_tests/SingnedOutStack.test.tsx
+++ __tests__/screen_tests/SingnedOutStack.test.tsx
@@ -2,11 +2,19 @@ import { render } from "@testing-library/react-native"
 import SingnedOutStack from "../../src/screens/SingnedOutStack"
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
+import ImageCropPicker, { openPicker } from 'react-native-image-crop-picker';
+import { Screen } from "react-native-screens";
 jest.mock('@react-navigation/native-stack', () => ({
   createNativeStackNavigator: jest.fn().mockImplementation(() => ({
     Navigator: jest.fn(),
+    Screen:jest.fn(),
   })),
+  Stack:jest.fn(),
 }));
+jest.mock('react-native-image-crop-picker',()=>({
+  ImageCropPicker:jest.fn(),
+  openPicker:jest.fn(),
+}))
 const mockedNavigate = jest.fn();
 const mockedGoBack = jest.fn();
 
