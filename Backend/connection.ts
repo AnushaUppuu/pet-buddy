ced71bc8024f50c9d46ec2d192df19e09dd6af7e
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-19 12:09:22 +0530

    Update the url password

--- Backend/connection.ts
+++ Backend/connection.ts
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 
 export async function connection() {
   try {
-    await mongoose.connect(`mongodb+srv://anushauppu:YzV84hrtsOm9Sr9c@cluster0.fsn9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    await mongoose.connect(`mongodb+srv://anushauppu:YzV84hrtsOm9Sr9C@cluster0.fsn9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     console.log('Connected to the database');
   } catch (e) {
-    console.log('Unable to connect');
+    console.log(e);
+    throw new Error("Unable to connect");
   }
 }
 export async function closeConnection() {
