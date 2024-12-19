c67773d10e5f7e14647024f81e9754e0736b9ed2
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-19 11:35:49 +0530

    Update the uri

--- Backend/connection.ts
+++ Backend/connection.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connection() {
   try {
-    await mongoose.connect(`mongodb+srv://anushauppu:${'Anu@2003'}@cluster0.fsn9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    await mongoose.connect(`mongodb+srv://anushauppu:${'YzV84hrtsOm9Sr9C'}@cluster0.fsn9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     console.log('Connected to the database');
   } catch (e) {
     console.log('Unable to connect');
