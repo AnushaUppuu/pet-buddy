0b79bdc6b670f9b0761f09572038f49fe7b1f438
AnushaUppuu <anusha.uppu@everest.engineering>
2025-01-05 19:49:24 +0530

    Add the youtube iframes

--- src/screens/Training.tsx
+++ src/screens/Training.tsx
@@ -1,52 +1,66 @@
-import React from 'react'
-import { Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-
+import React, {useState} from 'react';
+import {Linking, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import YoutubePlayer from 'react-native-youtube-iframe';
 function Training() {
-  const data=[
+  const [playing, setPlaying] = useState(true);
+  const data = [
     {
-      id:1,
-      name:"Helpful Dog Training",
-      Link:"https://www.ccpdt.org/resources/"
+      id: 1,
+      name: 'Helpful Dog Training',
+      Link: 'https://www.ccpdt.org/resources/',
+      youtubeLink: 'bsoN0Xgw44DvwC_v',
     },
     {
-      id:2,
-      name:"Kitten Behavior understand your kitten",
-      Link:"https://www.royalcanin.com/in/cats/kitten/understanding-your-kittens-behaviour?gclsrc=aw.ds&gad_source=1&gbraid=0AAAAADDBKrXmUSKUS5HUiSrzZcqGyErDp&gclid=EAIaIQobChMItYKy5J79iQMV6qtmAh1oOCBrEAAYASAAEgLHIPD_BwE"
-    }
-  ]
+      id: 2,
+      name: 'Kitten Behavior understand your kitten',
+      Link: 'https://www.royalcanin.com/in/cats/kitten/understanding-your-kittens-behaviour?gclsrc=aw.ds&gad_source=1&gbraid=0AAAAADDBKrXmUSKUS5HUiSrzZcqGyErDp&gclid=EAIaIQobChMItYKy5J79iQMV6qtmAh1oOCBrEAAYASAAEgLHIPD_BwE',
+      youtubeLink: 'bsoN0Xgw44DvwC_v',
+    },
+  ];
   return (
     <View style={styles.mainContainer}>
-        <Text style={styles.name} testID='heading'>Trainings</Text>
-        {data.map((resource)=>{
-          return(
-            <View style={styles.container}>
-              <Text style={styles.name} testID='resource-name'>{resource.name}</Text>
-              <TouchableOpacity onPress={()=>  Linking.openURL(`${resource.Link}`)}>
-              <Text style={styles.link} testID='link'>{resource.Link}</Text>
-              </TouchableOpacity>
-              
-              </View>
-          )
-        })}
+      <Text style={styles.name} testID="heading">
+        Trainings
+      </Text>
+      {data.map(resource => {
+        return (
+          <View style={styles.container}>
+            <Text style={styles.name} testID="resource-name">
+              {resource.name}
+            </Text>
+            <TouchableOpacity
+              onPress={() => Linking.openURL(`${resource.Link}`)}>
+              <Text style={styles.link} testID="link">
+                {resource.Link}
+              </Text>
+              <YoutubePlayer
+                height={300}
+                play={playing}
+                videoId={`${resource.youtubeLink}`}
+              />
+            </TouchableOpacity>
+          </View>
+        );
+      })}
     </View>
-  )
+  );
 }
-const styles=StyleSheet.create({
-  container:{
-    borderWidth:2,
-    padding:10,
-    borderRadius:10,
-    margin:10,
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 10,
+    margin: 10,
   },
-  mainContainer:{
-    gap:10,
+  mainContainer: {
+    gap: 10,
   },
-  name:{
-    fontWeight:'bold'
+  name: {
+    fontWeight: 'bold',
   },
-  link:{
-    color:'blue'
-  }
-})
+  link: {
+    color: 'blue',
+  },
+});
 
-export default Training
+export default Training;
